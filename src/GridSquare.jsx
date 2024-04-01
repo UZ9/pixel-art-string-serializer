e2e30a0717d7a7b6e9031378e644b99753d4f378
Yerti <36551149+UZ9@users.noreply.github.com>
2024-04-01 15:10:09 -0400

    Add editing functionality

--- src/GridSquare.jsx
+++ src/GridSquare.jsx
@@ -1,15 +1,19 @@
 import { useState } from "react";
 
-export default function GridSquare({ value}) {
+export default function GridSquare({ value, row, col, setGridCharacter }) {
   const [status, setStatus] = useState('red');
   
   const handleMouseDown = (event) => {
     setStatus(status === "red" ? "blue" : "red");
+
+    setGridCharacter(row, col, '');
   }
 
+
+
+
   return (
-    <div style={{backgroundColor: status, flex: 1}} onMouseDown={handleMouseDown}>
-      <span >{value}</span>
+    <div style={{backgroundColor: value, flex: 1}} onMouseDown={handleMouseDown}>
     </div>
   )
 }
--- src/PixelArtGrid.jsx
+++ src/PixelArtGrid.jsx
@@ -2,46 +2,79 @@ import { useEffect } from "react";
 import { useState } from "react";
 import GridSquare from "./GridSquare";
 
+const availableColors = ["red", "green", "blue", "black", "white"];
+
 export default function PixelArtGrid() {
   const [grid, setGrid] = useState([]);
+  const [currentColor, setCurrentColor] = useState("blue");
 
   useEffect(() => {
+    console.log("triggered useffect");
     const emptyGrid = [];
 
     for (let i = 0; i < 11; i++) {
       emptyGrid[i] = [];
 
       for (let j = 0; j < 11; j++) {
-        emptyGrid[i][j] = "L";
+        emptyGrid[i][j] = "red";
       }
     }
 
     setGrid(emptyGrid);
   }, []);
 
+  const setGridCharacter = (row, col, newValue) => {
+    let tempGrid = [...grid];
+
+    tempGrid[row][col] = currentColor;
+
+    setGrid(tempGrid);
+  };
+
   const generateString = () => {
     let output = "";
 
-    console.log({grid});
+    console.log({ grid });
 
-    grid.forEach(row => {
-      row.forEach(col => {
+    grid.forEach((row) => {
+      row.forEach((col) => {
         output += col;
-      })
-    })
+      });
+    });
 
     return output;
   };
 
+  console.log({ availableColors });
+
   return (
     <>
-      {" "}
+      <div style={{ margin: "2rem" }}>
+        {availableColors.map((color) => (
+          <button
+            key={color}
+            style={{ margin: "0rem 1rem 0rem 0rem", backgroundColor: color }}
+            onClick={() => setCurrentColor(color)}
+          >
+            {color}
+          </button>
+        ))}
+      </div>
+
       <div className="grid">
-        {grid.map((row, index) => {
+        {grid.map((row, rowIndex) => {
           return (
             <>
-              {grid[index].map((col) => {
-                return <GridSquare value={col} key={row + col} />;
+              {grid[rowIndex].map((col, colIndex) => {
+                return (
+                  <GridSquare
+                    setGridCharacter={setGridCharacter}
+                    row={rowIndex}
+                    col={colIndex}
+                    value={grid[rowIndex][colIndex]}
+                    key={rowIndex * 1000 + colIndex}
+                  />
+                );
               })}
             </>
           );
